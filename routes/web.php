89ad565abb684be36752ecfed4072c1bb13a2476
yokotechteam <146733706+yokotechteam@users.noreply.github.com>
2023-10-12 20:30:35 +0630

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ require_once "../vendor/autoload.php";
 use App\Controllers\User;
 // use App\Controllers\Page;
 
-die(var_dump($_SERVER['REQUEST_URI']))
+die(var_dump($_SERVER['REQUEST_URI']));
 $ROOT = '/task_manager/public/';
 
 $routes = [ 
