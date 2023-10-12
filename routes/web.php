427551aa7d150395c61e4929b7992cc44bf579d1
yokotechteam <146733706+yokotechteam@users.noreply.github.com>
2023-10-12 20:34:11 +0630

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ $routes = [
   '/email-verify' => [ User::class, 'email_verify' ]
 ];
 // $route  = str_replace ( $ROOT, '', $_SERVER[ 'REQUEST_URI' ] );
+// $route  = $_SERVER[ 'REQUEST_URI' ];
+
 // die( var_dump ( $route ) );
 if ( $route === '/' )
 {
